194964ede2b0e247e6339b6b5db718a3fb2e4ef7
jtrinidad679 <54329472+jtrinidad679@users.noreply.github.com>
2019-09-05 22:42:02 -0500

    Add files via upload

---
+++ 3.1.py
@@ -0,0 +1,18 @@
+user_number = int(input("Enter a number between 1 and 7: "))
+
+if user_number == 1:
+    print("Monday")
+elif user_number == 2:
+    print("Tuesday")
+elif user_number == 3:
+    print("Wednesday")
+elif user_number == 4:
+    print("Thursday")
+elif user_number == 5:
+    print("Friday")
+elif user_number == 6:
+    print("Saturday")
+elif user_number == 7:
+    print("Sunday")
+else:
+    print("Error: That number is not valid")
