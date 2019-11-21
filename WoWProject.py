e61a0e262501bbd33997a6b339ff96bcdf725f5b
jtrinidad679 <54329472+jtrinidad679@users.noreply.github.com>
2019-11-21 13:53:04 -0600

    Add files via upload

--- WoWProject.py
+++ WoWProject.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from PIL import Image, ImageTk
+from PIL import ImageTk
 import cv2
 
 
@@ -8,9 +8,9 @@ class MainScreen:
         self.master = master
         self.master.title('World of Warcraft')
 
-        # master.iconbitmap(r'wow_icon.png')
+        master.iconbitmap(r'wow_icon.png')
 
-        # image = cv2.imread('worldofwarcraft.jpg')
+        # image = cv2.imread('worldofwarcraft2.jpg')
         #
         # b, g, r = cv2.split(image)
         # img = cv2.merge((r, g, b))
@@ -19,43 +19,46 @@ class MainScreen:
         #
         # im = Image.fromarray(img)
         # imgtk = ImageTk.PhotoImage(image=im)
-        #
+        # #
         # Label(master, image=imgtk).pack()
+        canvas = Canvas(width=1250, height=700)
+        canvas.pack(expand=YES, fill=BOTH)
+        image = ImageTk.PhotoImage(file='worldofwarcraft2.jpg')
+        canvas.create_image(10, 10, image=image, anchor=NW)
+        master.mainloop()
+
+        # master.geometry("2000x2000")
+
+        self.character_frame = Frame(self.master)
+        self.create_character_frame = Frame(self.master)
+        self.quit_frame = Frame(self.master)
+
+        self.value = IntVar()
+        self.value.set(1)
+
+        self.character_button = Button(self.create_character_frame, text='Create New Character', command=self.faction_menu)
+        self.quit_button = Button(self.quit_frame, text='Quit', bg='black', fg='red', command=self.master.destroy)
+
+        self.character_button.pack(side='left')
+        self.quit_button.pack(side='left')
+
+        self.character_frame.pack()
+        self.create_character_frame.pack()
+        self.quit_frame.pack()
+
+    def faction_menu(self):
+        if self.value.get() == 1:
+            _ = Faction()
 
 
 def main():
     root = Tk()
     _ = MainScreen(root)
     root.mainloop()
-#
-#         # canvas = Tk.Canvas(master, width=2000, height=2000)
-#         # wow_image = Tk.PhotoImage(file="worldofwarcraft.jpg")
-#         # canvas.create_image(0,0,anchor=NW, image = wow_image)
-#         # canvas.pack()
-#         #
-#         # master.geometry("2000x2000")
-#         #
-#         # self.character_frame = tk.Frame(self.master)
-#         # self.create_character_frame = tk.Frame(self.master)
-#         # self.quit_frame = tk.Frame(self.master)
-#         #
-#         # self.value = tk.IntVar()
-#         # self.value.set(1)
-#         #
-#         # self.character_button = tk.Button(self.create_character_frame, text='Create New Character', command=self.faction_menu)
-#         # self.quit_button = tk.Button(self.quit_frame, text='Quit', bg='black', fg='red', command=self.master.destroy)
-#         #
-#         # self.character_button.pack(side='left')
-#         # self.quit_button.pack(side='left')
-#         #
-#         # self.character_frame.pack()
-#         # self.create_character_frame.pack()
-#         # self.quit_frame.pack()
-#
-#     # def faction_menu(self):
-#     #     if self.value.get() == 1:
-#     #         _ = Faction()
-#
+
+
+main()
+
 #
 # class Faction:
 #     def _init__(self, master):
