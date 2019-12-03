e738c4d69d2c4e5ae0fdb1e6e05769954c7114c2
jtrinidad679 <54329472+jtrinidad679@users.noreply.github.com>
2019-12-03 13:50:08 -0600

    Add files via upload

--- test.py
+++ test.py
@@ -7,26 +7,32 @@ class WoW:
     def __init__(self, master):
         self.master = master
         self.master.title("World of Warcraft")
-
         master.geometry('1000x700')
 
-        self.character_frame = Frame(self.master).grid(row=0, sticky=W)
-        self.create_character_frame = Frame(self.master).grid(row=3, sticky=W)
-        self.quit_frame = Frame(self.master).grid(row=4, sticky=W)
-        self.image_frame = Frame(self.master).grid(row=5, sticky=W)
+        self.character_label = Label(master, text='Your Characters', font=("Helvetica", 30))
+        self.character_label.place(anchor=NW)
+        self.character_button = Button(master, text='Create New Character', font=("Helvetica", 26),
+                                       fg='blue', command=self.faction_menu)
+        self.quit_button = Button(master, text='Quit', font=("Helvetica", 26),
+                                  fg='red', command=self.master.destroy)
 
-        self.character_label = Label(self.character_frame, text='Your Characters', font=("Helvetica", 30)).grid(row=0,
-                                                                                                                sticky=W)
+        self.character_button.place(relx=0, rely=1, anchor=SW)
+        self.quit_button.place(relx=1, rely=1, anchor=SE)
 
-        self.character_button = Button(self.create_character_frame, text='Create New Character', font=("Helvetica", 26),
-                                       command=self.faction_menu).grid(row=3, sticky=W)
-        self.quit_button = Button(self.quit_frame, text='Quit', font=("Helvetica", 26),
-                                  command=self.master.destroy).grid(row=4, sticky=W)
+        canvas = Canvas(master, width=500, height=625)
+        canvas.pack(side=RIGHT)
+        image = ImageTk.PhotoImage(file='wowza.jpg')
+        canvas.create_image(265, 200, image=image)
+        master.mainloop()
 
-    def faction_menu(self):
-        faction_window = Tk()
+    def faction_menu(master):
+        faction_window = Tk.Toplevel(master)
         faction_window.title("Faction Selector")
         faction_window.geometry('1000x700')
+
+        faction_description_label = Label(master,
+                                               text='Before you step foot into Azeroth, you must select a faction.'
+                                                    'The factions available to you are:', font=("Helvetica", 30))
 
 
 def main():
